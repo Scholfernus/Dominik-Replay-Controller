5ce38c5cef33737abf1b9fd42550e5887437c921
Scholfernus <48571340+Scholfernus@users.noreply.github.com>
2024-03-20 19:33:56 +0100

    Aplikacja startowa do aplikacji

---
+++ src/main/java/com/example/democontoller/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.example.democontoller;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(DemoControllerApplication.class);
+	}
+
+}
