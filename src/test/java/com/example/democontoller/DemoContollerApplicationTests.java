2faca9e73007bd0f75ff164d778edac364ab9e33
Scholfernus <48571340+Scholfernus@users.noreply.github.com>
2024-03-22 20:56:26 +0100

    testowanie

---
+++ src/test/java/com/example/democontoller/DemoContollerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.democontoller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoContollerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
